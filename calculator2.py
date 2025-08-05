9ebf6572502a6e2f9a574cc77c6bf441947c1fe9
shila-96 <chepshila96@gmail.com>
2025-08-05 15:55:18 +0300

    Initial commit

---
+++ calculator2.py
@@ -0,0 +1,22 @@
+num1 = 15
+num2 = 2
+operator = input("Enter the operation (+, -, *, /): ")
+
+# Perform the operation
+if operator == "+":
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+elif operator == "-":
+    result = num1 - num2
+    print(f"{num1} - {num2} = {result}")
+elif operator == "*":
+    result = num1 * num2
+    print(f"{num1} * {num2} = {result}")
+elif operator == "/":
+    if num2 != 0:
+        result = num1 / num2
+        print(f"{num1} / {num2} = {result}")
+    else:
+        print("Error: Division by zero is not allowed.")
+else:
+    print("Invalid operator.")
---
+++ index.py
@@ -0,0 +1,8 @@
+name="shila"
+
+age=26
+
+print(age,name)
+
+site_name="power learn project"
+print(site_name)
